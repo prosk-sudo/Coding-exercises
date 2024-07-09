e4b9bf74342d0e896535ad3f9cea18b19bde24bf
Jeong Jun Ahn <tyyyu1268@gmail.com>
2024-07-09 15:50:26 +0900

    1081~1085

---
+++ CodeUp/1083.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		if (i % 3 == 0) {
+			printf("X ");
+		} else {
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return 0;
+}
